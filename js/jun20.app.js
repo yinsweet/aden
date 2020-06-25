60ab0240513a96e41dccfa7cc22c7ca4362dd60f
yinsweet <yinsweet@gmail.com>
2020-06-26 01:55:21 +0800

    Create Jun20 photo gallery

---
+++ js/jun20.app.js
@@ -0,0 +1,391 @@
+const media = [
+  {
+    id: 1,
+    type: "picture",
+    date: "01/06/2020",
+    src: "./images/jun20/IMG-20200601-WA0000.jpg",
+  },
+  {
+    id: 2,
+    type: "video",
+    date: "01/06/2020",
+    src: "./images/jun20/VID-20200601-WA0002.mp4",
+  },
+  {
+    id: 3,
+    type: "video",
+    date: "01/06/2020",
+    src: "./images/jun20/VID-20200601-WA0003.mp4",
+  },
+  {
+    id: 4,
+    type: "video",
+    date: "01/06/2020",
+    src: "./images/jun20/VID-20200601-WA0004.mp4",
+  },
+  {
+    id: 5,
+    type: "video",
+    date: "01/06/2020",
+    src: "./images/jun20/VID-20200601-WA0005.mp4",
+  },
+  {
+    id: 6,
+    type: "video",
+    date: "03/06/2020",
+    src: "./images/jun20/VID-20200603-WA0000.mp4",
+  },
+  {
+    id: 7,
+    type: "picture",
+    date: "04/06/2020",
+    src: "./images/jun20/IMG-20200604-WA0000.jpg",
+  },
+  {
+    id: 8,
+    type: "picture",
+    date: "04/06/2020",
+    src: "./images/jun20/IMG-20200604-WA0001.jpg",
+  },
+  {
+    id: 9,
+    type: "picture",
+    date: "04/06/2020",
+    src: "./images/jun20/IMG-20200604-WA0002.jpg",
+  },
+  {
+    id: 10,
+    type: "video",
+    date: "05/06/2020",
+    src: "./images/jun20/VID-20200605-WA0001.mp4",
+  },
+  {
+    id: 11,
+    type: "video",
+    date: "08/06/2020",
+    src: "./images/jun20/VID-20200608-WA0000.mp4",
+  },
+  {
+    id: 12,
+    type: "video",
+    date: "08/06/2020",
+    src: "./images/jun20/VID-20200608-WA0001.mp4",
+  },
+  {
+    id: 13,
+    type: "video",
+    date: "08/06/2020",
+    src: "./images/jun20/VID-20200608-WA0002.mp4",
+  },
+  {
+    id: 14,
+    type: "video",
+    date: "08/06/2020",
+    src: "./images/jun20/VID-20200608-WA0003.mp4",
+  },
+  {
+    id: 15,
+    type: "picture",
+    date: "10/06/2020",
+    src: "./images/jun20/IMG-20200610-WA0000.jpg",
+  },
+  {
+    id: 16,
+    type: "video",
+    date: "11/06/2020",
+    src: "./images/jun20/VID-20200611-WA0000.mp4",
+  },
+  {
+    id: 17,
+    type: "video",
+    date: "11/06/2020",
+    src: "./images/jun20/VID-20200611-WA0001.mp4",
+  },
+  {
+    id: 18,
+    type: "video",
+    date: "11/06/2020",
+    src: "./images/jun20/VID-20200611-WA0002.mp4",
+  },
+  {
+    id: 19,
+    type: "video",
+    date: "13/06/2020",
+    src: "./images/jun20/VID-20200613-WA0000.mp4",
+  },
+  {
+    id: 20,
+    type: "video",
+    date: "14/06/2020",
+    src: "./images/jun20/VID-20200614-WA0000.mp4",
+  },
+  {
+    id: 21,
+    type: "video",
+    date: "15/06/2020",
+    src: "./images/jun20/VID-20200615-WA0003.mp4",
+  },
+  {
+    id: 22,
+    type: "picture",
+    date: "17/06/2020",
+    src: "./images/jun20/IMG-20200617-WA0010.jpg",
+  },
+  {
+    id: 23,
+    type: "picture",
+    date: "18/06/2020",
+    src: "./images/jun20/IMG-20200618-WA0002.jpg",
+  },
+  {
+    id: 24,
+    type: "picture",
+    date: "18/06/2020",
+    src: "./images/jun20/IMG-20200618-WA0003.jpg",
+  },
+  {
+    id: 25,
+    type: "picture",
+    date: "18/06/2020",
+    src: "./images/jun20/IMG-20200618-WA0005.jpg",
+  },
+  {
+    id: 26,
+    type: "video",
+    date: "18/06/2020",
+    src: "./images/jun20/VID-20200618-WA0004.mp4",
+  },
+  {
+    id: 27,
+    type: "picture",
+    date: "19/06/2020",
+    src: "./images/jun20/IMG-20200619-WA0008.jpg",
+  },
+  {
+    id: 28,
+    type: "picture",
+    date: "19/06/2020",
+    src: "./images/jun20/IMG-20200619-WA0009-COLLAGE.jpg",
+  },
+  {
+    id: 29,
+    type: "picture",
+    date: "19/06/2020",
+    src: "./images/jun20/IMG-20200619-WA0009.jpg",
+  },
+  {
+    id: 30,
+    type: "picture",
+    date: "19/06/2020",
+    src: "./images/jun20/IMG-20200619-WA0010.jpg",
+  },
+  {
+    id: 31,
+    type: "picture",
+    date: "19/06/2020",
+    src: "./images/jun20/IMG-20200619-WA0011.jpg",
+  },
+  {
+    id: 32,
+    type: "picture",
+    date: "19/06/2020",
+    src: "./images/jun20/IMG-20200619-WA0014.jpg",
+  },
+  {
+    id: 33,
+    type: "picture",
+    date: "19/06/2020",
+    src: "./images/jun20/IMG-20200619-WA0016.jpg",
+  },
+  {
+    id: 34,
+    type: "picture",
+    date: "21/06/2020",
+    src: "./images/jun20/IMG-20200621-WA0000.jpg",
+  },
+  {
+    id: 35,
+    type: "picture",
+    date: "21/06/2020",
+    src: "./images/jun20/IMG-20200621-WA0004.jpg",
+  },
+  {
+    id: 36,
+    type: "picture",
+    date: "21/06/2020",
+    src: "./images/jun20/IMG-20200621-WA0005.jpg",
+  },
+  {
+    id: 37,
+    type: "picture",
+    date: "21/06/2020",
+    src: "./images/jun20/IMG-20200621-WA0006.jpg",
+  },
+  {
+    id: 38,
+    type: "video",
+    date: "21/06/2020",
+    src: "./images/jun20/VID-20200621-WA0002.mp4",
+  },
+  {
+    id: 39,
+    type: "video",
+    date: "21/06/2020",
+    src: "./images/jun20/VID-20200621-WA0008.mp4",
+  },
+  {
+    id: 40,
+    type: "picture",
+    date: "22/06/2020",
+    src: "./images/jun20/IMG-20200622-WA0004.jpg",
+  },
+  {
+    id: 41,
+    type: "picture",
+    date: "22/06/2020",
+    src: "./images/jun20/IMG-20200622-WA0005.jpg",
+  },
+  {
+    id: 42,
+    type: "picture",
+    date: "22/06/2020",
+    src: "./images/jun20/IMG-20200622-WA0006.jpg",
+  },
+  {
+    id: 43,
+    type: "picture",
+    date: "22/06/2020",
+    src: "./images/jun20/IMG-20200622-WA0007.jpg",
+  },
+  {
+    id: 44,
+    type: "picture",
+    date: "22/06/2020",
+    src: "./images/jun20/IMG-20200622-WA0008.jpg",
+  },
+  {
+    id: 45,
+    type: "picture",
+    date: "22/06/2020",
+    src: "./images/jun20/IMG-20200622-WA0009.jpg",
+  },
+  {
+    id: 46,
+    type: "picture",
+    date: "22/06/2020",
+    src: "./images/jun20/IMG-20200622-WA0010.jpg",
+  },
+  {
+    id: 47,
+    type: "picture",
+    date: "23/06/2020",
+    src: "./images/jun20/IMG-20200623-WA0006.jpg",
+  },
+  {
+    id: 48,
+    type: "video",
+    date: "23/06/2020",
+    src: "./images/jun20/VID-20200623-WA0008.mp4",
+  },
+  {
+    id: 49,
+    type: "picture",
+    date: "24/06/2020",
+    src: "./images/jun20/IMG-20200624-WA0000.jpg",
+  },
+  {
+    id: 50,
+    type: "picture",
+    date: "24/06/2020",
+    src: "./images/jun20/IMG-20200624-WA0003.jpg",
+  },
+  {
+    id: 51,
+    type: "picture",
+    date: "24/06/2020",
+    src: "./images/jun20/IMG-20200624-WA0009.jpg",
+  },
+  {
+    id: 52,
+    type: "video",
+    date: "23/06/2020",
+    src: "./images/jun20/VID-20200624-WA0011.mp4",
+  },
+];
+
+
+const flexSection = document.querySelector('.flex-section');
+const btnContainer = document.querySelector('.btn-container');
+
+window.addEventListener('DOMContentLoaded', function(e) {
+  displayMediaItems(media);
+  displayMediaButtons();
+
+});
+
+function displayMediaItems(mediaItems){
+  let displayMedia = mediaItems.map(function(item){
+    // console.log(item);
+    if(item.type === "picture") {
+      return `  <img class="img-link img-thumb" src=${item.src} alt="Aden Wong">`;
+    } else {
+      return `<video class="img-thumb" controls>
+      <source src=${item.src} type="video/mp4">
+      Your browser does not support HTML video.
+    </video>`
+  }
+});
+
+displayMedia = displayMedia.join("");
+// console.log(displayMedia);
+flexSection.innerHTML = displayMedia;
+}
+
+function displayMediaButtons(){
+  // const dates = menu.map(function(item){
+  //   return item.category;
+  // });
+  const dates = media.reduce(function(values, item){
+    if(!values.includes(item.date)){
+      values.push(item.date);
+    }
+    return values;
+  },['all']);
+  // console.log(dates);
+  const dateBtns = dates.map(function (category){
+    return `<button class="filter-btn" type="button" data-date=${category}>${category}</button>`;
+  }).join("");
+
+  btnContainer.innerHTML = dateBtns;
+  const filterBtns = btnContainer.querySelectorAll('.filter-btn');
+  // console.log(dateBtns);
+  // filter items
+  filterBtns.forEach(function(btn){
+    btn.addEventListener('click', function (e) {
+      // console.log(e.currentTarget);
+      // console.log(e.currentTarget.dataset);
+      // console.log(e.currentTarget.dataset.id);
+      const date = e.currentTarget.dataset.date;
+      const mediaDate = media.filter(function (mediaItem){
+        // console.log(mediaItem.category);
+        if(mediaItem.date === date){
+          return mediaItem;
+        }
+      });
+      // console.log(mediaCategory);
+      if (date === 'all') {
+        displayMediaItems(media);
+      } else {
+        displayMediaItems(mediaDate);
+      }
+      // jquery code to open the BS4 modal
+      $("img").click(function(){
+        var img=$(this).attr('src');
+        $("#show-img").attr('src',img);
+        $("#imgmodal").modal('show');
+      });
+
+
+    });
+  });
+}
